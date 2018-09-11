411ab97301519afda2a01e6ec5eae6c103097d03
Drew Patel <drew1patel@gmail.com>
2018-09-11 08:34:50 -0500

    Initial Commit

---
+++ DecentSwift/DecentSwift.h
@@ -0,0 +1,19 @@
+//
+//  DecentSwift.h
+//  DecentSwift
+//
+//  Created by Drew Patel on 9/11/18.
+//  Copyright © 2018 Drew Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DecentSwift.
+FOUNDATION_EXPORT double DecentSwiftVersionNumber;
+
+//! Project version string for DecentSwift.
+FOUNDATION_EXPORT const unsigned char DecentSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DecentSwift/PublicHeader.h>
+
+
